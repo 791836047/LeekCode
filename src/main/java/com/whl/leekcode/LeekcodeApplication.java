9bed485ce1b5408eafc935ce5e8479a7fc69276c
liaowenhui <liaowenhui@nuomicidk.cn>
2021-12-23 18:11:25 +0800

    快速排序冒泡排序

---
+++ src/main/java/com/whl/leekcode/LeekcodeApplication.java
@@ -0,0 +1,13 @@
+package com.whl.leekcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeekcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeekcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whl/leekcode/LeekcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whl.leekcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeekcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
