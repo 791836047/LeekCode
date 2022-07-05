12aa2d5c720827a295c0257fa4f8a44aa1ffd7dc
liaowenhui <liaowenhui@nuomicidk.cn>
2022-07-05 14:36:38 +0800

    添加新

--- src/main/java/com/whl/leekcode/other/design/Singleton.java
+++ src/main/java/com/whl/leekcode/other/design/Singleton.java
@@ -6,16 +6,22 @@ package com.whl.leekcode.other.design;
  * @date 2022/4/14 9:51
  */
 public class Singleton {
+    /**
+     * 要点二:单例类必须自己创建自己的唯一实例。
+     * 具体实现二：创建一个该类的静态私有对象
+     */
     private static volatile Singleton singleton;
 
     /**
-     * 让构造函数为 private，这样该类就不会被实例化,外部new Singleton()会报错
+     * 要点一:单例类只能有一个实例。
+     * 具体实现一：让构造函数为 private，这样该类就不会被实例化.
      */
     private Singleton() {
     }
 
     /**
-     * 获取唯一可用的对象
+     * 要点三：单例类必须自行向整个系统提供这个实例。
+     * 具体实现三：提供了一个静态的公有的函数用于创建或获取它本身的静态私有对象。
      * @return
      */
     public static Singleton getSingleton() {
